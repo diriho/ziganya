d69583490336ce2c4485058bdc023d07c6dfaeef
Tharcisse Ntirandekura <tntirand@oberlin.edu>
2026-01-24 21:59:42 -0500

    Add Supabase integration and dashboard components; update project configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import TransactionLayer from "../sdk/dashboard";
+
 function App() {
   return(
     <div className='flex flex-col items-center justify-center h-screen bg-gray-100 p-4 m-4'>
       <h1 className='text-4xl font-bold text-gray-800 mb-4'>Ziganya</h1>
       <p className='text-gray-600'>Build for hackonomics 2026</p>
       <button className='bg-green-900 text-white px-6 py-2 rounded-md'>Click me</button>
+      <TransactionLayer/>
     </div>
   )
 }
